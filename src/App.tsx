a874c472d59e0456f08b3ffbc93bab9b15c11ec2
aiichiro <aiichirookuda@gmail.com>
2021-03-07 18:01:11 +0900

    データ型について学びました

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { StringLiteralLike } from 'typescript';
+
+const name = 'hello'; //リテラル型
+
+let username: string = 'Hello'; //文字列型
+let dummyNum: number = 2; //数値型
+let dool: boolean = true; //ブーリアン型
+// 上記のように明示的にデータ型を指定することをアノーテーションという。
+
+let array1 = [true, false, true];
+let array2 = [0, 1, 'hello']
+
+// オブジェクトの型の定義の方法
+interface NAME {
+  first: string
+  last: string
+}
+
+let nameObj: NAME = {first: 'Yamada', last: 'Taro'};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
